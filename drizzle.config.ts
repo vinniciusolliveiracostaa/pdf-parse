2bdf7254d15ef7b966e7f1e69b7639f4040293d9
Vinicius Oliveira <vinnicius.olliveira.costaa@outlook.com.br>
2025-11-15 13:25:27 -0300

    feat: ajusutando variaveis de ambiente para ambiente vercel + supabase

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
     out: './src/db/migrations',
   dialect: 'postgresql',
   dbCredentials: {
-    url: process.env.DATABASE_URL!,
+    url: process.env.POSTGRES_URL!,
   },
     casing: 'snake_case',
 });
